e820d125a6751ac211d761c6840c1aa2b76dadaf
Hyesun Yoo <hyesun@Hyesuns-MacBook-Pro.local>
2021-09-18 15:26:42 -0700

    fixed env

--- resources/item.py
+++ resources/item.py
@@ -42,8 +42,8 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
-            return {'message': 'Item deleted.'}
-        return {'message': 'Item not found.'}, 404
+
+        return {'message': 'Item deleted'}
 
     def put(self, name):
         data = Item.parser.parse_args()
