572f03fe14f008e1d3aec3b72ce958a9846798da
Hyesun Yoo <hyesun@Hyesuns-MacBook-Pro.local>
2021-09-18 14:50:39 -0700

    fixed env

--- app.py
+++ app.py
@@ -15,9 +15,10 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 # If DATABASE_URL is not available, use 'sqlite:///data.db' locally.
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+uri = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
